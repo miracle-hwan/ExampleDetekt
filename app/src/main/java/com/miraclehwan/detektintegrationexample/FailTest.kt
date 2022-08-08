83a70ae3b8be170012e2b5e5f0d8f1299d7a601d
miraclehwan <daehwan.kim@nrise.net>
2022-08-09 01:06:16 +0900

    add_FailTest.kt

---
+++ app/src/main/java/com/miraclehwan/detektintegrationexample/FailTest.kt
@@ -0,0 +1,9 @@
+package com.miraclehwan.detektintegrationexample
+
+data class FailTest (val a:Int,val  b:Boolean,
+
+                     val c:Int){
+    fun testtest( a:Int) {}
+
+    fun maxLineWarning_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890( a:Int) = Unit
+}
